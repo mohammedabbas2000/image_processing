70483e7d8b4f74c0c91de851f57a4715725b96ad
mohammedabbas2000 <73596848+mohammedabbbas@users.noreply.github.com>
2022-12-13 17:24:11 +0200

    lsat update

--- hw2/main.py
+++ hw2/main.py
@@ -20,7 +20,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(gray, 75, 200)
 
-contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 
 max_cont=contours[0]
@@ -28,8 +28,7 @@ for i in contours :
   if cv2.contourArea(max_cont)<cv2.contourArea(i):
      max_cont=i
 
-cv2.drawContours(image, max_cont, -1,  (255, 255, 255), 3)
-
+cv2.drawContours(image, max_cont, -1,  (0, 255, 0), 3) #sharpens the frame in green
 peri = cv2.arcLength(max_cont, True)
 approx = cv2.approxPolyDP(max_cont, 0.05 * peri, True)
 
@@ -42,4 +41,6 @@ M = cv2.getPerspectiveTransform(pts1, pts2)
 dst = cv2.warpPerspective(orginal, M, (cols, rows))
 
 dst = cv2.resize(dst,(rows,cols))
+cv2.imshow('image',dst)
+cv2.waitKey(0)
 cv2.imwrite(path_output_img,dst)
